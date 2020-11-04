c17be902b1723bf574ffaa817936e86253125361
Bro-Epitech <bgwenael007@gmail.com>
2020-11-04 20:06:34 +0100

    test

---
+++ projetCordova/www/precache-manifest.5540695e4023867020cfd1e184d5db27.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1cac7995e38918522ff",
+    "url": "css/about.bcdb6d59.css"
+  },
+  {
+    "revision": "e4e5a5c10b22b68b95b9",
+    "url": "css/app.601d2ada.css"
+  },
+  {
+    "revision": "a6cf1b1a32671505abd8",
+    "url": "css/chunk-vendors.a2cadc9e.css"
+  },
+  {
+    "revision": "a3eb0575e256771acbeb",
+    "url": "css/login.f11ec897.css"
+  },
+  {
+    "revision": "9bae8fba2555f2cf8561",
+    "url": "css/team.6acd1f0e.css"
+  },
+  {
+    "revision": "798625f3d4fd501041598eea96cc6bd5",
+    "url": "index.html"
+  },
+  {
+    "revision": "fb1817b96c65b6477cb55fedf53e86d9",
+    "url": "jquery-3.5.1.js"
+  },
+  {
+    "revision": "f1cac7995e38918522ff",
+    "url": "js/about.a9057f86.js"
+  },
+  {
+    "revision": "7887b8a670d7b0e79dc4",
+    "url": "js/about~team.0c8d139b.js"
+  },
+  {
+    "revision": "14619fc82a5d9c21f065",
+    "url": "js/admin.aea98a00.js"
+  },
+  {
+    "revision": "e4e5a5c10b22b68b95b9",
+    "url": "js/app.8576ab5f.js"
+  },
+  {
+    "revision": "4f5d3985d95747d4db27",
+    "url": "js/chunk-2d0b2535.b9f0f0b5.js"
+  },
+  {
+    "revision": "63cd3ade789a767f34d5",
+    "url": "js/chunk-54d1cf48.7693f76f.js"
+  },
+  {
+    "revision": "2f306517bf36cd64c711",
+    "url": "js/chunk-7f11bcfe.5c4e710b.js"
+  },
+  {
+    "revision": "a6cf1b1a32671505abd8",
+    "url": "js/chunk-vendors.a43ddda0.js"
+  },
+  {
+    "revision": "129fdd1f6630b784c7a9",
+    "url": "js/dashboard.4f200140.js"
+  },
+  {
+    "revision": "a3eb0575e256771acbeb",
+    "url": "js/login.473a91e1.js"
+  },
+  {
+    "revision": "a5b4c7c7579388d05b42",
+    "url": "js/report.92a02c65.js"
+  },
+  {
+    "revision": "9bae8fba2555f2cf8561",
+    "url": "js/team.9c4a5b26.js"
+  },
+  {
+    "revision": "6759b648254f427d6ad2253899322be8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "564974405307cf6b8dcbecd3d94af206",
+    "url": "raphael-min.js"
+  }
+]);
